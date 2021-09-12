dd4122f108b3a9c681c89bbb1060ab61428b9504
T1xAn <pyps47.pyps47@yandex.ru>
2021-09-12 23:53:48 +0300

    N2

--- Lab_2/Source.cpp
+++ Lab_2/Source.cpp
@@ -1,62 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <locale.h>
-int a[200][200], b[200][200], c[200][200], elem_c;
+#include <locale>
+int mas[1000000];
+int size = 1000000;
 
-int main(void)
+
+void shell(int* items, int count)
 {
-	setlocale(LC_ALL, "Russian");
-	setvbuf(stdin, NULL, _IONBF, 0);
-	setvbuf(stdout, NULL, _IONBF, 0);
-	time_t begin_clock, end_clock;
-	clock_t start, end; // объявляем переменные для определения времени выполнения
+    clock_t start, end;
+    int i, j, gap, k;
+    int x, a[5];
 
-	int i = 0, j = 0, r;
-	
+    a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+    start = clock();
+    for (k = 0; k < 5; k++) {
+        gap = a[k];
+        for (i = gap; i < count; ++i) {
+            x = items[i];
+            for (j = i - gap; (x < items[j]) && (j >= 0); j = j - gap)
+                items[j + gap] = items[j];
+            items[j + gap] = x;
+        }
+    }
+    end = clock();
+    double time = difftime(end, start) / CLOCKS_PER_SEC;
+    printf(" Время затраченное на сортировку Шелла  составило - %f секунд", time);
+}
 
-	
+void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1);
+{
+    int i, j;
+    int x, y;
 
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
-	while (i < 200)
-	{
-		while (j < 200)
-		{
-			a[i][j] = rand() % 100 + 1; // заполняем массив случайными числами
-			j++;
-		}
-		i++;
-	}
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
-	i = 0; j = 0;
-	while (i < 200)
-	{
-		while (j < 200)
-		{
-			b[i][j] = rand() % 100 + 1; // заполняем массив случайными числами
-			j++;
-		}
-		i++;
-	}
-	 start = clock();
-	for (i = 0; i < 200; i++)
-	{
-		for (j = 0; j < 200; j++)
-		{
-			elem_c = 0;
-			for (r = 0; r < 200; r++)
-			{
-				elem_c = elem_c + a[i][r] * b[r][j];
-				c[i][j] = elem_c;
-			}
-		}
-	}
-	end = clock();
-	double time = difftime(end, start) / CLOCKS_PER_SEC;
-	 printf(" Время премножения жвух массивов составило - %f секунд", time);
-	/*std::cout << "Вам понадобилось  "
-		<< difftime(end, start)
-		<< " секунд для ввода своего имени.n";*/
 
-	return(0);
+
+    i = left; j = right;
+
+    /* выбор компаранда */
+    x = items[(left + right) / 2];
+    do {
+        while ((items[i] < x) && (i < right)) i++;
+        while ((x < items[j]) && (j > left)) j--;
+
+        if (i <= j) {
+            y = items[i];
+            items[i] = items[j];
+            items[j] = y;
+            i++; j--;
+        }
+    } while (i <= j);
+
+    if (left < j) qs(items, left, j);
+    if (i < right) qs(items, i, right);
+}
+void main() {
+    setlocale(LC_ALL, "Russian");
+    for (int i = 0; i < 100000; i++) {
+        mas[i] = rand() % 100;
+    }
+    //
+    mas[0] = 1;
+    for (int i = 0; i < 1000000; i++) {
+        mas[i] = mas[i] + 1;
+    }
+    //
+    mas[0] = 10000000;
+    for (int i = 0; i < 1000000; i++) {
+        mas[i] = mas[i] - 1;
+    }
+    //
+    mas[0] = 1;
+    for (int i = 0; i < 500000; i++) {
+        mas[i] = mas[i] - 1;
+    }
+    for (int i = 500000; i < 1000000; i++) {
+        mas[i] = mas[i] - 1;
+    }
+    //
+    shell(mas, size);
+    clock_t start, end;
+    start = clock();
+    qs(mas, 0, size - 1);
+    end = clock();
+    double time = difftime(end, start) / CLOCKS_PER_SEC;
+    printf("\n Время затраченное на быструю сортировку составило - %f секунд", time);
+
 }
